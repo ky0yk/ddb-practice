93f5e1c0a814237df90a92963b1f9c0def50a8c0
ky0yk <40429527+ky0yk@users.noreply.github.com>
2022-10-12 23:15:06 +0900

    単体テストを追加

---
+++ test/infra/dbClient/v2/UserDBClientV2Spec.scala
@@ -0,0 +1,88 @@
+package infra.dbClient.v2
+
+import software.amazon.awssdk.services.dynamodb.model._
+import domain.User
+import org.specs2.mock.Mockito
+import play.api.test.PlaySpecification
+import infra.dbclients.v2.UserDBClientV2
+import org.specs2.mutable.BeforeAfter
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
+
+import java.net.URI
+import scala.jdk.CollectionConverters.MapHasAsJava
+
+// AWS SDK for Javaをスムーズに利用するためにimplicit conversionを利用
+import infra.dbclients.v2.TypeConverter._
+
+class UserDBClientV2Spec extends PlaySpecification with Mockito {
+
+  trait DBClients {
+    val table = "users"
+    val dynamoDbClient = DynamoDbAsyncClient
+      .builder()
+      .endpointOverride(URI.create("http://localhost:8000"))
+      .build()
+    val userDBClient = new UserDBClientV2(dynamoDbClient)
+    val ScanReq = ScanRequest.builder().tableName(table).build()
+  }
+
+  trait DBClientBeforeAfter extends BeforeAfter {
+    self: DBClients =>
+
+    override def before = ()
+
+    override def after = {
+      await(dynamoDbClient.scan(ScanReq)).items().toSeq.foreach { item =>
+        val deleteReq = DeleteItemRequest
+          .builder()
+          .tableName(table)
+          .key(Map("user_id" -> item.get("user_id")))
+          .build()
+        dynamoDbClient.deleteItem(deleteReq)
+      }
+    }
+  }
+
+  trait UserArgs {
+    val user = User("userKey", "John", 50)
+  }
+
+  "ユーザーの作成" >> {
+
+    "テーブルに同じIDが存在しない場合" should {
+
+      class Context extends DBClientBeforeAfter with UserArgs with DBClients
+
+      "成功する" in new Context {
+        val future = userDBClient.create(user)
+        await(future) must not(throwA[Throwable])
+      }
+
+      "アイテムが1つDDBに作成される" in new Context {
+        val future = userDBClient.create(user)
+        await(future)
+        await(dynamoDbClient.scan(ScanReq)).items().toSeq.length mustEqual 1
+      }
+
+      "" in new Context {
+        val future = userDBClient.create(user)
+        await(future)
+
+        val key =
+          Map("user_id" -> AttributeValue.builder().s(user.id).build()).asJava
+        val getReq = GetItemRequest
+          .builder()
+          .tableName(table)
+          .key(key)
+          .build()
+        val item = await(dynamoDbClient.getItem(getReq))
+
+        item.item().get("user_name").s() mustEqual user.name
+        item.item().get("user_age").n().toInt mustEqual user.age
+
+      }
+
+    }
+
+  }
+}
